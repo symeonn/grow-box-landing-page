9cb9486e7969f24db5e048c823ef99cc0eb67bde
mario <symeonn@gmail.com>
2020-07-21 10:51:24 +0200

    Put units in current state data name #2

--- js/grow-box.js
+++ js/grow-box.js
@@ -40,9 +40,9 @@
     document.getElementById("datetime").innerHTML = json_obj.datetime
     document.getElementById("voc").innerHTML = json_obj.voc
     document.getElementById("co2").innerHTML = json_obj.co2
-    document.getElementById("humidity").innerHTML = json_obj.humidity.toFixed(2) + ' %'
-    document.getElementById("temperature").innerHTML = json_obj.temperature + ' °C'
-    document.getElementById("water_temperature").innerHTML = json_obj.water_temperature + ' °C'
+    document.getElementById("humidity").innerHTML = json_obj.humidity.toFixed(2)
+    document.getElementById("temperature").innerHTML = json_obj.temperature.toFixed(2)
+    document.getElementById("water_temperature").innerHTML = json_obj.water_temperature.toFixed(2)
     document.getElementById("ph").innerHTML = json_obj.ph
     document.getElementById("ec").innerHTML = json_obj.ec
     document.getElementById("local_ip").innerHTML = json_obj.local_ip
